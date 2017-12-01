23b6ca61ac0d3709500f149cd990656a63ec8356
Lisa Lee <snickers495@gmail.com>
2017-12-01 20:26:56 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,8 +1,8 @@
 class Student < ActiveRecord::Base
-  def index 
+  def index
     @students = Student.all
     render "index"
-  end 
+  end
   def to_s
     self.first_name + " " + self.last_name
   end
